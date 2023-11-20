86249820c3e7c5fb6769fc0e3ef11c803672ecd5
Lavanya <lavanya.goel12@gmail.com>
2023-11-20 15:25:26 -0500

    debugging

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -14,7 +14,8 @@ import {useState, useEffect} from "react";
 function Courses() {
   const { courseId } = useParams();
   const { pathname } = useLocation();
-  const URL = "http://localhost:4000/api/courses";
+  const API_BASE = process.env.REACT_APP_API_BASE;
+  const URL = `${API_BASE}/api/courses`;
   const [course, setCourse] = useState({});
   const findCourseById = async (courseId) => {
     const response = await axios.get(
